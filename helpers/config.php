48e205eaf92f940e0da05f12e468087c309e659d
23jn0336 <23jn0336@jec.ac.jp>
2025-01-29 12:16:50 +0900

    Update config.php

--- helpers/config.php
+++ helpers/config.php
@@ -1,5 +1,5 @@
 <?php
     // DB接続設定
-    define('DSN', 'sqlsrv:server = tcp:23jn0336db.database.windows.net,1433; Database = 23jn0336DB');
+    define('DSN',"sqlsrv:server = tcp:23jn0336db.database.windows.net,1433; Database = 23jn0336DB");
     define('DB_USER', '23jn0336'); 
     define('DB_PASSWORD', '23jn0336');　
